c1e9155e6ae635de38dc274ec0185b41fe108736
Eyasin Sheikh Rony <100122939+EyasinSheikhRony@users.noreply.github.com>
2022-10-20 21:16:09 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Profile-Site
-Profile
+# Site
---
+++ site.js
@@ -0,0 +1,25 @@
+let num = ["apple", "orange", "pine Apple", "Banana"];
+
+// for (let fruite of num) {
+//     console.log(fruite);
+// }
+
+// for (let f = 0; f < num.length; f++) {
+//     console.log(num[f]);
+// }
+
+// num.forEach((x) => {
+//     console.log(x);
+// });
+
+// let t = (x) => {
+//     console.log(`sq ${x} : ${x*x}`);
+// }
+
+// let y = t;
+
+// function high(n,c) {
+//     c(n);
+// }
+
+// high(6,y);
